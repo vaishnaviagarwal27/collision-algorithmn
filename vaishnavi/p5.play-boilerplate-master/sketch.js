7e231853545cce9b53fc0603c004e87bf7aa329b
vaishnaviagarwal27 <61281973+vaishnaviagarwal27@users.noreply.github.com>
2020-02-22 16:41:22 +0530

    Add files via upload

---
+++ vaishnavi/p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+  createCanvas(1200,800);
+  fixedRect=createSprite(600,400,60,80);
+  fixedRect.shapeColor="green";
+  movingRect=createSprite(800,200,80,30);
+  movingRect.shapeColor="green";
+
+  movingRect.debug=true;
+  fixedRect.debug=true;
+}
+
+function draw() {
+  background(0,0,0);  
+  
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY;
+
+  if(movingRect.x-fixedRect.x<fixedRect.width/2+movingRect.width/2 &&
+    fixedRect.x-movingRect.x<fixedRect.width/2+movingRect.width/2 &&
+    movingRect.y-fixedRect.y<fixedRect.height/2+movingRect.height/2 &&
+    fixedRect.y-movingRect.y<movingRect.height/2+fixedRect.height/2){
+    movingRect.shapeColor="red";
+    fixedRect.shapeColor="red";
+  }
+
+  else{
+    movingRect.shapeColor="green";
+    fixedRect.shapeColor="green";
+  }
+  drawSprites();
+}
